fabd6e2f66876ec72459c39466f999da53c1c93c
rbjornstad <roger.bjornstad@nav.no>
2019-11-29 09:53:03 +0100

    Adding shell-scripts for checking uptime via up

---
+++ check_uptime.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "Checking uptimed.sh - $(date)"
+if [ -d /proc/$(cat pid) ]; then
+    echo "uptimed.sh [$(cat pid)] is still running...";
+    kill $(cat pid)
+    exit 0;
+else
+    echo "uptimed.sh is not running...";
+    cat ./uptimed_last_output
+    echo "----- nohup.out -----"
+    cat nohup.out
+    exit 1;
+fi
